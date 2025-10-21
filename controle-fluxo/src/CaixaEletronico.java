6aeab64ff65bca34207c55c0748d0d9a0a1bb8f3
IRobertDF <trebor.org@gmail.com>
2025-10-21 10:07:08 -0300

    Criando novo método e atualizando condições CaixaEletronico

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -3,9 +3,11 @@ public class CaixaEletronico{
         double saldo = 25.0;
         double valorSolicitado = 17.0;
 
-        if(valorSolicitado < saldo)
+        if(valorSolicitado < saldo){
         saldo = saldo - valorSolicitado;
+        System.out.println("Novo Saldo: " + saldo);
 
-        System.out.println(saldo);
+        }else
+        System.out.println("Saldo Insuficiente");
     }
 }
