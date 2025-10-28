064c5424301409ecef786593d3658f915f177b71
Robert <144074614+IRobertDF@users.noreply.github.com>
2025-10-28 11:32:46 -0300

    uptade

--- controle-fluxo/src/ExemploForEach.java
+++ controle-fluxo/src/ExemploForEach.java
@@ -1,7 +1,7 @@
 public class ExemploForEach{
 public static void main(String[] args) {
     //em arrays o indice inicia em zero
-    String alunos [] = {"Felipe", "Jonas", "Julia", "Marcos"};
+    String []alunos  = {"Felipe", "Jonas", "Julia", "Marcos"};
 
     for (String aluno : alunos){
         System.out.println("O nome do aluno é: " + aluno);
